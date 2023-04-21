7f3136955de1ff951f3f9203e68fa9905a418897
k022g0060@g.neec.ac.jp <k022g0060@g.neec.ac.jp>
2023-04-21 22:54:04 +0900

    PShlslにConstantBufferを記述

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,12 +1,25 @@
-struct PixelShaderOutput {
-	float32_t4 color : SV_TARGET0;
+struct Material {
+	float32_t4 color;
 };
-
+ConstantBuffer<Material> gMaterial : register(b0);
+struct PixelShaderOutput {
+	float32_t4 colotr : SV_TARGET0;
+};
 PixelShaderOutput main() {
 	PixelShaderOutput output;
-	output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
-	return output;
+	output.color = gMarareal.color;
 }
+
+
+//struct PixelShaderOutput {
+//	float32_t4 color : SV_TARGET0;
+//};
+//
+//PixelShaderOutput main() {
+//	PixelShaderOutput output;
+//	output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+//	return output;
+//}
 
 
 
